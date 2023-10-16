4e63274c2e3d716a4c428e4a99f559a35b58a797
JuanLaborde <juan.laborde33@gmail.com>
2023-10-16 13:25:27 +0200

    Upload Page

---
+++ src/data/constants.jsx
@@ -0,0 +1,322 @@
+export const Bio = {
+  name: "Juan Martin Laborde",
+  roles: [
+    "Frontend Developer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/JuanLaborde",
+  resume:
+    "https://drive.google.com/file/d/1kYsPqU1qzlIP-hMQpjTE6tvpQAbqxYij/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/juan-martin-laborde-64a401215",
+  insta: "https://www.instagram.com/juanmartin432/",
+  facebook: "https://www.facebook.com/profile.php?id=1249519188",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Ruby on Rails",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Ruby_On_Rails_Logo.svg/411px-Ruby_On_Rails_Logo.svg.png",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwindcss",
+        image:
+          "https://pbs.twimg.com/profile_images/1468993891584073729/a_op8KnL_400x400.jpg",
+      },
+
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Cloudinary",
+        image:
+          "https://media.licdn.com/dms/image/D4E0BAQHWZAjYRxtfgg/company-logo_200_200/0/1688138388687?e=1704931200&v=beta&t=BWKHE5wAOy5kQIR19_hpTmiEd4_eRE5Oxw_YUU4FjuA",
+      },
+      {
+        name: "Visual Studio 2022",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Visual_Studio_Icon_2022.svg/193px-Visual_Studio_Icon_2022.svg.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+      {
+        name: "C#",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/C_Sharp_Icon.png/120px-C_Sharp_Icon.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C560BAQHNqzWMWUqV3g/company-logo_200_200/0/1677676022958?e=1704931200&v=beta&t=L3M_aQa1twLSkmoubzMBsSZyfSCf8I0xi4uMDhG5o54",
+    role: "Technical Support",
+    company: "Sitel Group, Berlin Germany",
+    date: "January 2022 - May 2023",
+    desc: "Direct Phone Service with the Customer Playstation Project. Handling Calls, Chats, and Emails in English and Spanish.",
+
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/D4E0BAQGTUswcRlgg9A/company-logo_200_200/0/1689352303421?e=1704931200&v=beta&t=FEgfoUOk1_lEwpMGsrgQtD0cJdI6VYVAzTc1U_1LeKs",
+    role: "Team Leader",
+    company: "Accenture SRL, Buenos Aires Argentina",
+    date: "December 2014 - December 2020",
+    desc: "Technology Support Service Desk Buenos Aires - Internal Project. Generation of reports (Excel and Power BI) and presentations (PowerPoint) on a global scale. Coordination of tasks and personnel management (>20 team members). Internal and global team meetings. Responsible for developing and implementing internal training processes for the team and newly hired personnel. Responsible for spearheading automation projects utilizing differents programming languages (implemented automations used in various projects worldwide).",
+    skills: [
+      "RPA",
+      "C#",
+      "Python",
+      "GitHub",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/D560BAQEZ6ZVjZmwiwA/company-logo_200_200/0/1692312131153?e=1704931200&v=beta&t=HCjMESYQi55iazLd9PsZq8ZwVv2vWH-41pwpMcjA7lo",
+    role: "Second Level Technical Support (RDM - GSD)",
+    company: "HP - Hewlett-Packard, Buenos Aires Argentina",
+    date: "May 2011 - September 2014",
+    desc: "Second-level customer service and issue resolution. Responsible for tracking and following up on issues until they are resolved. Analysis of metrics and managing improvements in project production. Development and implementation of internal training processes for newly hired personnel. Responsible for standardizing processes and enhancing internal communication (Harmony project implementation).",
+    skills: [
+      "Excel",
+      "Word",
+      "Outlook",
+      "PowerPoint",
+      "SAP",
+      "AS400",
+    ],
+  },
+  {
+    id: 3,
+    img: "https://media.licdn.com/dms/image/D560BAQGiz5ecgpCtkA/company-logo_200_200/0/1688684715427?e=1704931200&v=beta&t=cQnJ9lCzAorij6Jf3YXPqT1hwn_BSU6STEXj90D7Mic",
+    role: "Second Level Technical Support",
+    company: "IBM SRL, Buenos Aires Argentina",
+    date: "January 2010 - April 2011",
+    desc: "Direct phone service with the customer. Responsible for the creation and standardization of processes. Collaboration in the transition process of the helpdesk from Brazil to Argentina.",
+  },
+
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C4E0BAQEMcw7g_5Et_A/company-logo_200_200/0/1654683278845?e=1704931200&v=beta&t=EQbJZ_WVLYhrPTxnDZP_BZnmOxIDUwwT_i_xXslMQwE",
+    school: "Le Wagon - Bootcamp",
+    date: "May 2023 - Present",
+    desc: "I started this bootcamp, on the recommendation of the Agentur für Arbeit, since it is a Speak and Code program, which includes an integration course, a German language course, and full-stack web development with Ruby on Rails.",
+    degree: "FullStack Developer with Ruby on Rails",
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/C4E0BAQHvAypiry8QaA/company-logo_200_200/0/1634842512403?e=1704931200&v=beta&t=y-3-7i_pzeLeD7pk48FL5U1RimHsWP4Fx9BeWJKZEi0",
+    school: "Technological University of Buenos Aires (UTN)",
+    date: "August 2023 - October 2023",
+    desc: "I started this diploma as a way to further enhance my knowledge in the field of programming. We learned various methods for conducting different types of software testing with the aim of identifying and resolving bugs in them.",
+    degree: "Diploma in Expert Software Testing",
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/C4D0BAQGexnfBxeEG-g/company-logo_200_200/0/1608039227697?e=1704931200&v=beta&t=u6YDRFVHEe5ALUkktYzrlUrkpoAIW5QCcZMWW4nXI3Q",
+    school: "Coursera Google Certification",
+    date: "January 2023 - March 2023",
+    desc: "I joined this course through INCO, a German institution that prepares people for various job areas. The course consisted of 6 modules, each requiring several hours to complete within 3 months, and I successfully completed it on time, earning certificates for each module.",
+    degree: "IT Automation with Python, Google Certified",
+  },
+  {
+    id: 3,
+    img: "https://media.licdn.com/dms/image/C4E0BAQHvAypiry8QaA/company-logo_200_200/0/1634842512403?e=1704931200&v=beta&t=y-3-7i_pzeLeD7pk48FL5U1RimHsWP4Fx9BeWJKZEi0",
+    school: "Technological University of Buenos Aires (UTN)",
+    date: "August 2022 - February 2023",
+    desc: "I began this FullStack web development diploma with React with the intention of formalizing my knowledge in web development programming. I delivered a final project that included a homepage, four sections, a contact form, and a backend for news, and I received the highest grade in the course.",
+    degree: "Diploma in Full-Stack Web Programming with React JS",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Old Game Tribute",
+    date: "January 2023 - February 2023",
+    description:
+      "This was the first project I worked on for the FullStack diploma with React JS. The assignment was to create a page with 4 sections, a contact form, and a part that would work with the backend. I decided to use the theme of video games and built this page, which earned me the highest grade in the course.",
+    image: "https://res.cloudinary.com/dqhtvtptp/image/upload/v1697061147/oldgametribute_vfudey.png",
+    tags: [
+      "React", "HTML", "CSS", "JS", "Express JS", "Node JS", "React Js", "Email JS", "MySql",],
+    category: "web app",
+    github: "https://github.com/JuanLaborde/GameOldTribute",
+    webapp: "https://oldgametribute.onrender.com/",
+  },
+  {
+    id: 0,
+    title: "Restaurant Page",
+    date: "May 2023",
+    description:
+      "This page was created with a focus on the Frontend. I created it to practice what I've learned and to learn how to use various libraries, AOS or Framer-Motion. The restaurant doesn't exist, so what is shown is fictional.",
+    image:
+      "https://res.cloudinary.com/dqhtvtptp/image/upload/v1697151299/Restaurant_spxfbh.png",
+    tags: ["React Js", "AOS", "Framer-Motion", "HTML", "CSS"],
+    category: "web app",
+    github: "https://github.com/JuanLaborde/RestaurantPage",
+    webapp: "https://restaurantpage-69oy.onrender.com/",
+  },
+  {
+    id: 1,
+    title: "Cool Page",
+    date: "August 2023",
+    description:
+      "This page was created with the simple intention of practicing the use of the GSAP library. The page consists of basic HTML, CSS, and JavaScript. However, through the GSAP library, a scroll animation is achieved, making it quite interesting.",
+    image:
+      "https://res.cloudinary.com/dqhtvtptp/image/upload/v1697156678/CoolPage2023_iynblu.png",
+    tags: ["HTML", "JS", "CSS", "GSAP",],
+    category: "web app",
+    github: "https://github.com/JuanLaborde/CoolPage",
+    webapp: "https://coolanimationpage.onrender.com/",
+  },
+  {
+    id: 2,
+    title: "Metaversus",
+    date: "April 2023",
+    description:
+      "I created this page to practice with Next.js and Tailwind CSS. The page was built using augmented reality as a foundation and the exploration of fictional worlds. The information on the page is not real.",
+    image: "https://res.cloudinary.com/dqhtvtptp/image/upload/v1697158334/Metaverse_wkxman.png",
+    tags: ["Next JS", "Tailwindcss", "Framer Motion", "JS", "HTML"],
+    category: "web app",
+    github: "https://github.com/JuanLaborde/Metaversus",
+    webapp: "https://metaversus-5wb7.onrender.com/",
+  },
+  {
+    id: 3,
+    title: "3D Portfolio",
+    date: "September 2023",
+    description:
+      "I created this alternative portfolio with the idea of using the Three.js library, which can be seen in use on the computer that is rendered, as well as the world that is seen next to the contact form. The information displayed on that page is not real, but it demonstrates how the page works.",
+    image:
+      "https://res.cloudinary.com/dqhtvtptp/image/upload/v1697172711/Portfolio3d_bwcllz.png",
+    tags: ["React JS", "Vite", "Three JS", "Tailwindcss", "Email JS", "Framer Motion"],
+    category: "web app",
+    github: "https://github.com/JuanLaborde/3dPortfolio",
+    webapp: "https://threedportfolio-tgv4.onrender.com/",
+  },
+  {
+    id: 10,
+    title: "Bank Modern Page",
+    date: "August 2023",
+    description:
+      "This page was designed for a bank or a financial institution. The page is built in React and Tailwind CSS. The information displayed on the page is fictitious, and I created it to practice React and the use of Tailwind CSS.",
+    image:
+      "https://res.cloudinary.com/dqhtvtptp/image/upload/v1697329676/ModernBankApp_zhz3dv.png",
+    tags: ["React JS", "Vite", "Tailwindcss", "AOS"],
+    category: "web app",
+    github: "https://github.com/JuanLaborde/BankModernApp",
+    webapp: "https://bankmodernapp.onrender.com/",
+  },
+  {
+    id: 5,
+    title: "GPT3-JSM",
+    date: "July 2023",
+    description:
+      "Based on the excitement that ChatGPT from OpenAI generated, I built this page in React as part of my React practice. The information on the page is not real; the goal is to work on the visual and design aspects.",
+    image:
+      "https://res.cloudinary.com/dqhtvtptp/image/upload/v1697335074/JSMGPT3_v2m1rs.png",
+    tags: ["React Js", "Javascript", "CSS", "HTML"],
+    category: "web app",
+    github: "https://github.com/JuanLaborde/GPT3_JSM",
+    webapp: "https://gpt3-jsm.onrender.com/",
+  },
+  {
+    id: 6,
+    title: "YouTube Clone",
+    date: "May 2023",
+    description:
+      "The idea of this page was to clone YouTube and practice using APIs (in this case, I used RapidAPI). I used Material UI for all the components. The page is functional, and you can search for videos, channels, and play them.",
+    image:
+      "https://res.cloudinary.com/dqhtvtptp/image/upload/v1697408696/CloneYoutube_v04jiv.png",
+    tags: ["React Js", "RapidAPI", "Axios", "Material UI"],
+    category: "web app",
+    github: "https://github.com/JuanLaborde/YouTubeClone",
+    webapp: "https://youtubeclone-e3s0.onrender.com/",
+  },
+  {
+    id: 8,
+    title: "Coming soon",
+    date: "",
+    description:
+      "",
+    image:
+      "https://media.gettyimages.com/id/1331245557/de/video/4k-coming-soon-neon-sign-stil-blinkend-animation-der-zahlenbewegung.jpg?s=640x640&k=20&c=C_utDSS2wAHdCc9D2W8go1RG6qRBvGP9piZMWoiuysU=",
+      tags: ["Python, Open CV"],
+    category: "machine learning",
+    github: "",
+    webapp: "",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
